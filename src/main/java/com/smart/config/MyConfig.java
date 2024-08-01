e6d29bc357d576f8bf99a8cd7a53c7400f12d628
Omkarthorat1042 <omkarthorat1042@gmail.com>
2024-08-01 18:52:12 +0530

    Initial commit of Smart Contact Manager project

---
+++ src/main/java/com/smart/config/MyConfig.java
@@ -0,0 +1,43 @@
+package com.smart.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class MyConfig extends WebSecurityConfiguration {
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		
+		
+		
+		
+		
+		
+		return new UserDetailServiceImp();
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		
+		
+		return new BCryptPasswordEncoder();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
